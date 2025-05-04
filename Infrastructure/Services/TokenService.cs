41d12e233ff7ebcc23f04b0742d3d92cb6c47ffa
AhmadSharairi <shraere24@gmail.com>
2025-05-04 19:09:57 +0300

    Add User.Id, GivenName, and Surname to token claims

--- Infrastructure/Services/TokenService.cs
+++ Infrastructure/Services/TokenService.cs
@@ -25,9 +25,10 @@ namespace Infrastructure.Services
         {
             
             var claims = new List<Claim>
-            {
+            {   new Claim(ClaimTypes.NameIdentifier, user.Id),
                 new Claim(ClaimTypes.Email, user.Email),
-                new Claim(ClaimTypes.GivenName, user.DisplayName)
+                new Claim(ClaimTypes.GivenName, user.FirstName),
+                new Claim(ClaimTypes.Surname, user.LastName)
             };
 
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
